c28e8440f080bd41f931616d9e426569ebc79376
Dokutah-san <114378296+Dokutah-san@users.noreply.github.com>
2022-09-26 11:48:12 +0700

    Add files via upload

---
+++ JAVASCRIPT/js/script.js
@@ -0,0 +1,20 @@
+// var nama="Reggy";
+// var umur= 17;
+// document.write("Nama:" + nama + "Feraldin" + "<br>");
+// document.write("Umur:" + umur);
+// alert("Welcome to Goolagoon");
+// var ytb = confirm("apakah anda ingin buka youtube?");
+
+// if(ytb){
+//     window.location = "https://www.youtube.com";
+// }
+// else {
+//     document.write("ok,disini aja🗿")
+// }
+
+// var nama = prompt("Siapa nama kamu?:");
+// document.write("Hello nama kamu " + nama + " ya?")
+
+var alas =8;
+var tinggi =8;
+var hasil = 1/2 * alas * tinggi;
